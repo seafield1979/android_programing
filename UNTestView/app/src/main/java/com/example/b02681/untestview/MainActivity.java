e0db3ae83746c6108aec47a2a949fe678795e8ec
Shusuke Unno <unno_shunsuke_gn@cyberagent.co.jp>
2015-04-24 19:22:42 +0900

    レイアウト確認用のサンプル追加

---
+++ UNTestView/app/src/main/java/com/example/b02681/untestview/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.b02681.untestview;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+    public static int requestCode = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickButton1(View v){
+        // Activity1を起動
+        startActivityForResult(
+                new Intent(this,Activity1.class),
+                requestCode);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
